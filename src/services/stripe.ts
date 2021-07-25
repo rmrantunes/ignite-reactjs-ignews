1408cfd9e2703c97796d99f8ffc0853762fa1eb4
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-25 15:08:49 -0400

    typo: stripe env variable

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 import { name, version } from "../../package.json";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+export const stripe = new Stripe(process.env.STRAPI_API_SECRET_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
     name,

3b4fbafd3098baa8b0612231971833c58dd29fd6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-25 15:16:56 -0400

    feat: Home to get revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "components/SubscribeButton";
 
+import { stripe } from "services/stripe";
+
 import avatarImg from "assets/images/avatar.svg";
 import styles from "styles/Home.module.scss";
 
-export default function Home() {
+type HomePageProps = {
+  product: {
+    priceId: string;
+    priceText: string;
+  };
+};
+
+export default function Home(props: HomePageProps) {
   return (
     <>
       <Head>
@@ -20,14 +30,34 @@ export default function Home() {
           </h1>
 
           <p>
-            Get acess to all the publications <span>for $9,90/month</span>
+            Get acess to all the publications{" "}
+            <span>for {props.product.priceText}/month</span>
           </p>
 
           <SubscribeButton />
         </div>
-
         <Image src={avatarImg} alt="Girl coding and chilling" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve(
+    process.env.STRAPI_PRODUCT_PRICE_ID,
+    { expand: ["product"] }
+  );
+
+  const product = {
+    priceId: price.id,
+    priceText: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};

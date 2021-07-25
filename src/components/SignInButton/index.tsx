57bbfe44d590d088a87573fd56547ea7d57c023f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-25 17:17:38 -0400

    feat: add session logic to SignInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,39 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import styles from "./SignInButton.module.scss";
 import { FaGithub } from "react-icons/fa";
 
-export function SignInButton() {
+type SignInButtonProps = {
+  isLoggedIn: boolean;
+  username?: string;
+  onSignIn: () => void;
+  onSignOut: () => void;
+};
+
+export function SignInButton(props: SignInButtonProps) {
+  const handleSignAction = props.isLoggedIn ? props.onSignOut : props.onSignIn;
+  const githubIconFillColor = props.isLoggedIn ? "#04D361" : "#EBA417";
+
   return (
-    <button className={styles.signInButton}>
-      <FaGithub fill="#EBA417" />
-      <span>Sign in with GitHub</span>
+    <button className={styles.signInButton} onClick={handleSignAction}>
+      <FaGithub fill={githubIconFillColor} />
+      <span>
+        {props.isLoggedIn
+          ? props.username ?? "You're logged in"
+          : "Sign in with GitHub"}
+      </span>
     </button>
+  );
+}
+
+export function SignInButtonLogicBoundary() {
+  const [session] = useSession();
+
+  return (
+    <SignInButton
+      isLoggedIn={Boolean(session)}
+      onSignIn={() => signIn("github")}
+      onSignOut={signOut}
+    />
   );
 }

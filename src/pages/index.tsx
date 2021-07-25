bfb5709a5552d553c5ce031ef6b470657ae3157e
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-25 11:56:22 -0400

    feat: add Home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import avatarImg from "assets/images/avatar.svg";
+import styles from "styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -6,6 +10,21 @@ export default function Home() {
       <Head>
         <title>Home | ignews</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <div className={styles.hero}>
+          <span>👏🏽 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+
+          <p>
+            Get acess to all the publications <span>for $9,90/month</span>
+          </p>
+        </div>
+
+        <Image src={avatarImg} alt="Girl coding and chilling" />
+      </main>
     </>
   );
 }

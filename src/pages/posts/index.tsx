fddfba0f656420ee6d9e52d77612c73b640d02b6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-26 15:22:23 -0400

    feat: get static posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { getPrismicClient } from "services/prismic";
+import { RichText } from "prismic-dom";
+
 import styles from "./Posts.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostsPageProps = {
+  posts: Post[];
+};
+
+export default function Posts(props: PostsPageProps) {
   return (
     <>
       <Head>
@@ -20,3 +36,30 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    { fetch: ["post.title", "post.content"], pageSize: 100 }
+  );
+
+  const posts = response.results.map((post) => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt:
+      post.data.content.find((content) => content.type === "paragraph")?.text ??
+      "",
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  }));
+
+  return { props: { posts }, revalidade: 60 * 60 };
+};

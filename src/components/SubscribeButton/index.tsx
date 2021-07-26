0a865deee4736e6c61e07f4c9841a66371e101ff
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-26 16:18:58 -0400

    feat: activeSubscription guard to handleSubscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/dist/client/router";
+
 import { makeAPIRoutesRequest } from "services/axios";
 import { getStripeJs } from "services/stripe-js";
 
@@ -23,9 +25,16 @@ export function SubscribeButton(props: SubscribeButtonProps) {
 export function SubscribeButtonLogicBoundary() {
   const [session] = useSession();
 
+  const router = useRouter();
+
   async function handleSubscribe() {
     if (!session) {
       signIn("github");
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push("/posts");
       return;
     }
 

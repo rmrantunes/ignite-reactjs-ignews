ae3bfed180d3e4b0beac7da69d7940a69e63861b
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-20 15:02:03 -0400

    chore: moduleNameMapper to handle not JS imports

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/src/tests/setup-tests.ts'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '.(scss|css|sass)': 'identity-obj-proxy',
+    'components/(.+)$': '<rootDir>/src/components/$1',
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/src/tests/__mocks__/fileMock.js`
+  }
 }

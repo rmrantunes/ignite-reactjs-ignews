bf5a0db690d79392a3b8707f954a3c5b59dce01d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-26 08:57:43 -0400

    test: Home page initial data

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -1,5 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../../pages/index'
+import { stripe } from '../../services/stripe'
+import { mocked } from 'ts-jest/utils'
+import Home, { getStaticProps } from '../../pages/index'
+
+jest.mock('../../services/stripe')
 
 describe('<Home /> page', () => {
   it('should render correcly', () => {
@@ -11,5 +15,27 @@ describe('<Home /> page', () => {
     expect(
       screen.getByRole('img', { name: /Girl coding and chilling/i })
     ).toBeInTheDocument()
+  })
+
+  it('should get initial data', async () => {
+    const mockedRetrieveStripePrices = mocked(stripe.prices.retrieve)
+
+    mockedRetrieveStripePrices.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000
+    } as any)
+
+    const initialData = await getStaticProps({})
+
+    expect(initialData).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            priceText: '$10.00'
+          }
+        }
+      })
+    )
   })
 })

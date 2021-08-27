5e5b1ce77bd1c924744de753dd686c4e570c6513
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-27 10:57:33 -0400

    fix: pages/posts styles import

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,22 +1,22 @@
-import { GetStaticProps } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import Prismic from "@prismicio/client";
-import { getPrismicClient } from "services/prismic";
-import { RichText } from "prismic-dom";
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Prismic from '@prismicio/client'
+import { getPrismicClient } from 'services/prismic'
+import { RichText } from 'prismic-dom'
 
-import styles from "./Posts.module.scss";
+import styles from 'styles/Posts.module.scss'
 
 type Post = {
-  slug: string;
-  title: string;
-  excerpt: string;
-  updatedAt: string;
-};
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
 
 type PostsPageProps = {
-  posts: Post[];
-};
+  posts: Post[]
+}
 
 export default function Posts(props: PostsPageProps) {
   return (
@@ -39,32 +39,32 @@ export default function Posts(props: PostsPageProps) {
         </div>
       </main>
     </>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient();
+  const prismic = getPrismicClient()
 
   const response = await prismic.query(
-    [Prismic.predicates.at("document.type", "post")],
-    { fetch: ["post.title", "post.content"], pageSize: 100 }
-  );
+    [Prismic.predicates.at('document.type', 'post')],
+    { fetch: ['post.title', 'post.content'], pageSize: 100 }
+  )
 
   const posts = response.results.map((post) => ({
     slug: post.uid,
     title: RichText.asText(post.data.title),
     excerpt:
-      post.data.content.find((content) => content.type === "paragraph")?.text ??
-      "",
+      post.data.content.find((content) => content.type === 'paragraph')?.text ??
+      '',
     updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-      "pt-BR",
+      'pt-BR',
       {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
       }
-    ),
-  }));
+    )
+  }))
 
-  return { props: { posts }, revalidade: 60 * 60 };
-};
+  return { props: { posts }, revalidate: 60 * 60 }
+}
